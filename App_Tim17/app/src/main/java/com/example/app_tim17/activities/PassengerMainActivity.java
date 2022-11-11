d5215f4474085685421fd2d9198a46ce38f7b1a8
Katarina Mirkovic <mirkovicka01@gmail.com>
2022-11-12 00:21:30 +0100

    Add missing string values and Override methods

--- App_Tim17/app/src/main/java/com/example/app_tim17/activities/PassengerMainActivity.java
+++ App_Tim17/app/src/main/java/com/example/app_tim17/activities/PassengerMainActivity.java
@@ -3,6 +3,7 @@ package com.example.app_tim17.activities;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.example.app_tim17.R;
@@ -37,5 +38,40 @@ public class PassengerMainActivity extends AppCompatActivity implements BottomNa
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         return false;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "onRestart()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "onPause()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "onStop()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
