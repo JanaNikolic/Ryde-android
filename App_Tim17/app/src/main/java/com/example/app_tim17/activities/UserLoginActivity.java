5baacbbd40262f48967bc8ba643143aa3fb900a0
Jana Nikolic <jananikolic25@gmail.com>
2022-11-11 21:44:37 +0100

    Connected UserLoginActivity with PassengerMainActivity and PassengerRegisterActivity

--- App_Tim17/app/src/main/java/com/example/app_tim17/activities/UserLoginActivity.java
+++ App_Tim17/app/src/main/java/com/example/app_tim17/activities/UserLoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.app_tim17.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +17,22 @@ public class UserLoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+        Button loginBtn = findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(UserLoginActivity.this, PassengerMainActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button registerBtn = findViewById(R.id.registerBtn);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(UserLoginActivity.this, PassengerRegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
     @Override
     protected void onStart() {
