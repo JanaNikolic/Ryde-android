ee8338adc3274b982cb3849bd2dcaf58c2c54dc5
Katarina Mirkovic <mirkovicka01@gmail.com>
2022-11-12 23:31:33 +0100

    Create PassengerInboxActivity and populate ListView with custom Adapter

--- App_Tim17/app/src/main/java/com/example/app_tim17/activities/PassengerMainActivity.java
+++ App_Tim17/app/src/main/java/com/example/app_tim17/activities/PassengerMainActivity.java
@@ -1,5 +1,6 @@
 package com.example.app_tim17.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,6 +23,7 @@ public class PassengerMainActivity extends AppCompatActivity implements BottomNa
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passenger_main);
         bottomNavigationView = findViewById(R.id.nav_view);
+        bottomNavigationView.setSelectedItemId(R.id.home);
         bottomNavigationView.setOnItemSelectedListener(this);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -35,39 +37,53 @@ public class PassengerMainActivity extends AppCompatActivity implements BottomNa
     }
 
 
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.inbox:
+                startActivity(new Intent(getApplicationContext(), PassengerInboxActivity.class));
+                overridePendingTransition(0, 0);
+                return true;
+            case R.id.home:
+                return true;
+            case R.id.history:
+                startActivity(new Intent(getApplicationContext(), PassengerMainActivity.class));
+                overridePendingTransition(0, 0);
+                return true;
+            case R.id.profile:
+                startActivity(new Intent(getApplicationContext(), PassengerMainActivity.class));
+                overridePendingTransition(0, 0);
+                return true;
+        }
         return false;
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Toast.makeText(this, "onStart()", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Toast.makeText(this, "onRestart()", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Toast.makeText(this, "onResume()", Toast.LENGTH_SHORT).show();
+        bottomNavigationView = findViewById(R.id.nav_view);
+        bottomNavigationView.setSelectedItemId(R.id.home);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Toast.makeText(this, "onPause()", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Toast.makeText(this, "onStop()", Toast.LENGTH_SHORT).show();
     }
 
     @Override
