73aa89000c8b359a5dccb096b48ca882c791c503
Jana Nikolic <jananikolic25@gmail.com>
2022-11-13 21:45:15 +0100

    Created CardView for ride history

--- App_Tim17/app/src/main/java/com/example/app_tim17/activities/DriverRideHistoryActivity.java
+++ App_Tim17/app/src/main/java/com/example/app_tim17/activities/DriverRideHistoryActivity.java
@@ -2,6 +2,7 @@ package com.example.app_tim17.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,22 +13,49 @@ import com.example.app_tim17.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class DriverRideHistoryActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
-
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_ride_hystory);
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.nav_view_history);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.nav_view_history);
         bottomNavigationView.setSelectedItemId(R.id.history);
-//        bottomNavigationView.setOnItemSelectedListener(this);
+        bottomNavigationView.setOnItemSelectedListener(this);
+//        CardView cardView = (CardView) findViewById(R.id.card_view);
 
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.menu_header, menu);
+        getMenuInflater().inflate(R.menu.menu_driver, menu);
         setTitle(R.string.ride_history);
         return true;
     }
+
+//    public void onBackPressed() {
+//        super.onBackPressed();
+//    }
+//
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        bottomNavigationView = findViewById(R.id.nav_view_history);
+//        bottomNavigationView.setSelectedItemId(R.id.history_driver);
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//    }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -44,7 +72,7 @@ public class DriverRideHistoryActivity extends AppCompatActivity implements Bott
                 overridePendingTransition(0, 0);
                 return true;
             case R.id.profile:
-                startActivity(new Intent(getApplicationContext(), DriverMainActivity.class));
+                startActivity(new Intent(getApplicationContext(), DriverAccountActivity.class));
                 overridePendingTransition(0, 0);
                 return true;
         }
