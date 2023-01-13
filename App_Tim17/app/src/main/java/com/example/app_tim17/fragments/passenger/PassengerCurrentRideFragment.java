a78b91d0b0ba45fd31edca330dd8073369167425
Jana Nikolic <jananikolic25@gmail.com>
2023-01-13 15:30:36 +0100

    Fixed adding ids in layouts

--- App_Tim17/app/src/main/java/com/example/app_tim17/fragments/passenger/PassengerCurrentRideFragment.java
+++ App_Tim17/app/src/main/java/com/example/app_tim17/fragments/passenger/PassengerCurrentRideFragment.java
@@ -17,50 +17,28 @@ import com.example.app_tim17.R;
  */
 public class PassengerCurrentRideFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public PassengerCurrentRideFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PassengerCurrentRideFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PassengerCurrentRideFragment newInstance(String param1, String param2) {
+    public static PassengerCurrentRideFragment newInstance() {
         PassengerCurrentRideFragment fragment = new PassengerCurrentRideFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_passenger_current_ride, container, false);
+        View view = inflater.inflate(R.layout.fragment_passenger_current_ride, container, false);
+
+        return view;
     }
 }
